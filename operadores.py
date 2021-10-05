e1ae6e27d37968e000a67f9e61ba70f1fccd56a1
Davi Eduardo <davieduardo94@gmail.com>
2021-10-05 15:51:44 -0400

    Submit
    Informações sobre os operadores em Python

---
+++ operadores.py
@@ -0,0 +1,21 @@
+# Operador de igualdade ==
+# Quando queremos comparar dois valores, usamos o operador de igualdade, que é: ==
+
+# Operador de diferente: !=
+# O contrário do operador 'igual a' é o operador 'diferente de', representado por !=
+# Este operador compara duas coisas, e retorna True se forem diferentes e False se forem iguais.
+
+# Operador maior que: >
+# Esse e os próximos, você vai se lembrar da escola.
+# > é o operador 'maior que'.
+
+# Operador menor que: <
+# Ao fazer: x < y , estamos perguntando ao Python se x é menor que y.
+# Se ele for menor mesmo, retorna True.
+
+# x == y         x é igual a y ?
+# x != y         x é diferente de y ?
+# x  > y         x é maior que y?
+# x  < y         x é menor que y ?
+# x >= y         x é maior ou igual a y ?
+# x <= y         x é menor ou igual a y ?
