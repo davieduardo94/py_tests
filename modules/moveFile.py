6da49b91c92abb710a7d2a9deb27835443e547bf
Davi Eduardo <davieduardo94@gmail.com>
2021-10-17 22:36:39 -0400

    Submit moveFile
    scritp para mover arquivo de pasta com py

---
+++ modules/moveFile.py
@@ -0,0 +1,20 @@
+# caminho onde vai salvar para presencial
+sourcePres = 'C:\Temp\Pres\\'
+# caminho onde vai salvar para ead
+sourceEAD = 'C:\Temp\EAD\\'
+# pasta onde será baixado o arquivo csv
+downFile = 'C:\Temp\CSV\\'
+# nome dos arquivos
+fileEAD = 'EAD.zip'
+filePres = 'Pres.zip'
+
+def moveFile(account):
+	print('Movendo arquivo para pasta selecionada')
+	if(account==215):
+		destination = sourcePres
+	else:
+		destination = sourceEAD
+	fileIn = downFile
+	dataDir = os.listdir(fileIn)	
+	for fileInFolder in dataDir:
+			shutil.move(fileIn+fileInFolder,destination)
