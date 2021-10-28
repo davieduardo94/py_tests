476605762ea16e742b0b601779d0a82ed3ed50d0
Davi Eduardo <davieduardo94@gmail.com>
2021-10-28 19:04:54 -0400

    Update examples

--- tutorials/lista_2.py
+++ tutorials/lista_2.py
@@ -23,3 +23,19 @@ print('Pop:', semana)
 
 semana.reverse()
 print('Reverse:',semana)
+
+
+# unsando IN
+# SE 'Sexta' está na lista semana
+if 'Sexta' in semana:
+    print("Sim, ", 'Sexta'," está na lista")
+else:
+    print("Não está")
+    
+
+#usando NOT IN
+# SE 'Domingo' não está na lista semana
+if 'Domingo' not in semana:
+    print("Não, ", 'Domingo'," não está na lista")
+else:
+    print("Está na lista")
