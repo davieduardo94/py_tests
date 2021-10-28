335b6628393b196f836ce7857f48506c3400a66c
Davi Eduardo <davieduardo94@gmail.com>
2021-10-28 18:11:59 -0400

    Update exmaple of Lists.

--- tutorials/lista_2.py
+++ tutorials/lista_2.py
@@ -1,9 +1,13 @@
-# Exemplos de funçoes com listas em Pythob
 semana=['Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado', 'Domingo' ]
 
 print('Tamanho:', len(semana))
 print('repetiçoes:', semana.count('primeiro'))
 print('Lista:',semana)
+
+print('Fracionando 1: até fim', semana[1:])
+print('Fracionando :5 até começo', semana[:5])
+print('Fracionando 0:3', semana[0:3])
+
 
 semana.append('Folga')
 print('Adicinar:', semana)
