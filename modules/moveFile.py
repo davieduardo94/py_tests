59cfd72daa0bb7a8000c0111ac2564d25fd5c285
Davi Eduardo <davieduardo94@gmail.com>
2022-01-02 21:56:48 -0400

    Updated moveFile.py to fix #3
    This update provides a move of files with a specified name upon function execution

--- modules/moveFile.py
+++ modules/moveFile.py
@@ -6,9 +6,9 @@ destination = 'C:\Temp\\'
 # pasta onde será baixado o arquivo csv
 downloadFile = 'C:\Temp\CSV\\'
 
-def moveFile(account):
+def moveFile(fileName,extension):
 	print('Movendo arquivo para pasta selecionada')
 # 	listando arquivos no diretorio
-	dataDir = os.listdir(downloadFile)	
+	dataDir = glob.glob(os.path.join(os.path.expanduser('~'), f'{downloadFile}\\{fileName}*'+extension))
 	for fileInFolder in dataDir:
 		shutil.move(downloadFile+fileInFolder,destination)
